5007d550dfea0c95dbefc2188ac9d50357fd8be0
Péres <davi5211peres@gmail.com>
2026-06-01 14:03:35 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -233,44 +233,6 @@ def validation(nome,idade,curso):
     
     return True
  
-def validarProf(professor, turmaP, idadeP, materia):
-  
-    if professor.strip() == "":
-       print("erro encontrado (1)")
-       time.sleep(3)
-       return
- 
-    if turmaP.strip() == "":
-       print("erro encontrado (2)")
-       time.sleep(3)
-       return False 
-    
-    if idadeP.strip() == "":
-       print("erro encontrado (3)")
-       time.sleep(3)
-       return False
-    
-    if materia.strip() == "":
-       print("erro encontrado (4)")
-       time.sleep(3)
-       return False
-    
-    if not professor.isalpha():
-        print("erro encontrado (5)")
-        time.sleep(3)
-        return False
-
-    if not materia.isalpha():
-        print("erro encontrado (6)")
-        time.sleep(3)
-        return False
-
-    if not turmaP.isdigit() or not idadeP.isdigit():
-        print("erro encontrado (7)")
-        time.sleep(3)
-        return False
-    return True
-
 
 
 def lista():
