8cebea465984edda763f86421556d690a91eba6d
★Caio Gomes★ <caiogbcorrea791@gmail.com>
2026-06-03 13:25:38 -0300

    Update

--- ProjetoKMB.py
+++ ProjetoKMB.py
@@ -1,3 +1,2 @@
 from menus import *
-
 login()
--- menus.py
+++ menus.py
@@ -6,6 +6,7 @@ from def_loading import *
 from defs_validacoes import *
 from defs_cadastros import *
 from defs_listagens import *
+from defs_deletar import *
 
 delay = random.randint(1 , 2)
 alunosC = []
@@ -24,67 +25,6 @@ def notas():
      print("escolha um aluno existente")
      return
 
-def deletarAluno():
-   conn = conectar()
-   cursor = conn.cursor()
-
-   id_deletar = input("digite o ID do aluno: ")
-
-   if not id_deletar.isdigit():
-      print("digite um ID válido")
-      time.sleep(3)
-      return
-
-   try:
-      cursor.execute("DELETE FROM alunos WHERE id_aluno = %s",(id_deletar,))
-      conn.commit()
-
-      if cursor.rowcount > 0:
-         print("aluno deletado")
-         time.sleep(3)
-         return
-      else:
-         print("nenhum aluno com esse ID")
-         time.sleep(3)
-         return
-   except Error as e:
-      print(f"erro ao deletar aluno: {e}")
-      time.sleep(5)
-      return
-   finally:
-      cursor.close()
-      conn.close()
-
-def deletarProfessor():
-   conn = conectar()
-   cursor = conn.cursor()
-
-   try:
-      id_deletar = int(input("digite o ID do professor: "))
-   except ValueError:
-      print("digite um ID válido")
-      time.sleep(3)
-      return
-
-   try:
-      cursor.execute("DELETE FROM professores WHERE id_professor = %s",(id_deletar,))
-      conn.commit()
-
-      if cursor.rowcount > 0:
-         print("professor deletado")
-         time.sleep(3)
-         return
-      else:
-         print("nenhum professor com esse ID")
-         time.sleep(3)
-         return
-   except Error as e:
-      print(f"erro ao deletar professor: {e}")
-      time.sleep(5)
-      return
-   finally:
-      cursor.close()
-      conn.close()
 
 def menuProf():
     while True:
