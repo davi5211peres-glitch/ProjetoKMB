58c1fc7abb1f6772fcad385c24737772725a3f9d
Péres <davi5211peres@gmail.com>
2026-06-11 15:44:25 -0300

    fiz a validação da idade pro professor tbm
    copilot demorou pra pensar nisso...

--- defs_validacoes.py
+++ defs_validacoes.py
@@ -5,16 +5,14 @@ def validation(nome,idade,id_curso):
         print("erro encontrado (1)")
         time.sleep(3)
         return False
-    
-    if idade.strip() == "":
-        print("erro encontrado (idade)")
-        time.sleep(3)
-        return False
-    
     try:
         idade = int(idade)
+
     except ValueError:
-        print("erro encontrado (idade)")
+        print("erro encontrado (idadeT)")
+        time.sleep(3)
+        return False
+        
 
     if idade >= 21:
         print("erro encontrado (idade)")
@@ -25,66 +23,65 @@ def validation(nome,idade,id_curso):
        print("erro encontrado (2)")
        time.sleep(3)
        return False
-    
+
 
     if not nome.replace(" ", "").isalpha():
-        print("erro encontrado (3)")
+        print("erro encontrado (4)")
         time.sleep(3)
         return False
     
     if not id_curso.isdigit():
-       print("erro encontrado (4)")
+       print("erro encontrado (5)")
        time.sleep(3)
        return False
     
     return True
  
-def validarProf(professor, idadeP, materia, id_cursoP):
+def validarProf(professor, idadeP, materia, cursoP):
   
     if professor.strip() == "":
        print("erro encontrado (1)")
        time.sleep(3)
        return
     
-    if idadeP.strip() == "":
-       print("erro encontrado (idade)")
-       time.sleep(3)
-       return False
-
     try:
         idadeP = int(idadeP)
-    except ValueError:
-        print("erro encontrado (idade)")
 
+    except ValueError:
+        print("erro encontrado (idadeT)")
+        time.sleep(3)
+        return False
+    
     if idadeP <= 21:
         print("erro encontrado (idade)")
         time.sleep(3)
         return False
  
-    if id_cursoP.strip() == "":
+    if cursoP.strip() == "":
        print("erro encontrado (2)")
        time.sleep(3)
        return False 
     
     
     if materia.strip() == "":
-       print("erro encontrado (3)")
+       print("erro encontrado (4)")
        time.sleep(3)
        return False
     
     if not professor.replace(" ", "").isalpha():
-        print("erro encontrado (4)")
-        time.sleep(3)
-        return False
-
-    if not materia.replace(" ", "").isalpha():
         print("erro encontrado (5)")
         time.sleep(3)
         return False
 
-    if not id_cursoP.isdigit():
+    if not materia.replace(" ", "").isalpha():
         print("erro encontrado (6)")
         time.sleep(3)
         return False
+
+    if not cursoP.replace(" ", "").isalpha():
+        print("erro encontrado (7)")
+        time.sleep(3)
+        return False
     
+
     return True
